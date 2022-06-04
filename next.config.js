e5a90e87b73e563f12095feb6158c1bc3d8212af
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-06-04 15:26:18 -0400

    add nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://kaushal135.github.io/pagesTestGithub' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/pagesTestGithub' : '',
   trailingSlash: true
 }
 
