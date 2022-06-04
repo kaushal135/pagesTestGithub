9f9273b18b6e4b793198a883586141cf7610b0cc
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-06-04 14:19:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pagestest/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/pagestes' : '',
   trailingSlash: true
 }
 
