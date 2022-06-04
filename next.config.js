b3cdfb652a3b0a35bf12ad7139acd0e7b3a3be9a
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-06-04 15:13:51 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pagesTestGithub' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://kaushal135.github.io/pagesTestGithub' : '',
   trailingSlash: true
 }
 
