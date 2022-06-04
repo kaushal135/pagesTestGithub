79bec75c5faaaca2a83ab4579c3edb412a3c2b85
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-06-04 15:07:04 -0400

    Update Meta.js

--- components/Meta.js
+++ components/Meta.js
@@ -1,22 +1,23 @@
 import Head from "next/head"
+import { assetPrefix } from '../next.config'
 
-const Meta = ({title, keywords, description}) => {
+const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
       <meta charSet='utf-8' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/favicon.ico' as={`${assetPrefix}/favicon.ico'`} />
       <title>{title}</title>
     </Head>
   )
 }
 
 Meta.defaultProps = {
-    title: 'Webdev Newz',
-    keywords: 'Web development, programming',
-    description: 'Get the latest news in web dev'
+  title: 'Webdev Newz',
+  keywords: 'Web development, programming',
+  description: 'Get the latest news in web dev'
 }
 
 export default Meta
